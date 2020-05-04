ff5da8e612923c4a1f775795c9883081ec5ca2c2
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-05-05 00:11:47 +0100

    Responsive & Styles

--- src/views/Wishes.vue
+++ src/views/Wishes.vue
@@ -1,30 +1,37 @@
 <template>
-  <v-row cols="10">
-    <v-col v-for="(event, idx) in events" :key="idx" cols="4">
-      <v-card class="mx-auto" outlined>
+<v-container>
+  <v-row>
+    <v-col v-for="(event, idx) in events" :key="idx" cols="12">
+      <v-card :to="{ name: 'BuyTicket', params: { eventId: event._id } }" class="mx-auto" outlined>
+       <v-list-item-title class="text-center headline card-title font-weight-regular">{{event.name}}</v-list-item-title>
         <v-list-item three-line>
+          <v-row>
+            <v-col cols="6">
           <v-list-item-content>
-            <v-list-item-title class="headline mb-1">{{event.name}}</v-list-item-title>
-            <v-divider></v-divider>
             <v-list-item-subtitle
               v-if="event.event_type"
-              class="headline mb-1"
-            >Type: {{event.event_type.name}}</v-list-item-subtitle>
-            <v-list-item-subtitle>
-              Date: {{event.date_start}}
+              class="body-1 mb-1"
+            >{{event.small_description}}</v-list-item-subtitle>
+          </v-list-item-content>
+            </v-col>
+            <v-col cols="6" class="text-end">
+            <v-list-item-subtitle class="title font-weight-bold">
+              {{event.date_start}}
               <span v-if="event.date_end">{{event.date_end}}</span>
             </v-list-item-subtitle>
-            <v-list-item-subtitle class="headline mb-1">Price: {{event.price}} €</v-list-item-subtitle>
-          </v-list-item-content>
+            </v-col>
+          </v-row>
         </v-list-item>
-        <v-card-actions>
-          <v-btn text color="primary" @click="buyEvent(events[idx]._id, events[idx])">Buy now</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn text color="red" @click="deleteById(events[idx]._id, events[idx])">Delete</v-btn>
+        <v-card-actions class="text-center">
+
+          <v-btn text color="primary" @click="buyEvent(events[idx]._id, events[idx])"><v-icon x-large color="cyan darken-4">mdi-cart</v-icon>Buy now</v-btn>
+            <v-list-item-subtitle class="headline">{{event.price}} €</v-list-item-subtitle>
+          <v-btn text color="red" @click="deleteById(events[idx]._id, events[idx])"><v-icon x-large>mdi-delete-circle</v-icon>Delete</v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
+</v-container>
 </template>
 
 <script>
@@ -55,4 +62,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.card-title{
+  background: rgb(21, 91, 100);
+  color: white;
+}
 </style>
