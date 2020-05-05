2d9de17506c9d6cf01eb6e70c9b53f26b259dba9
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-05-05 17:54:46 +0100

    VUE APP API URL

--- src/services/App.js
+++ src/services/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'VUE_APP_API_URL',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
