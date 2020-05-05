adca336bd29fd2626e9167408e759d2295f6c575
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-05-05 18:25:24 +0100

    process.env

--- src/services/App.js
+++ src/services/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'VUE_APP_API_URL',
+  baseURL: process.env.VUE_APP_API_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
